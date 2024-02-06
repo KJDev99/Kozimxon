1186da71b77dbf01ae932d77ecc058991291b4e5
Jamshid Qayimov <jamshidqayimov0399@gmail.com>
2024-02-06 23:41:05 +0500

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-[100%] bg-gray-500 h-[10vh]">
-    <div class="container mx-auto ">sas </div>
-  </div>
+  <headerVue />
+  <main />
 </template>
 
 <script setup>
-
+import headerVue from './components/header.vue';
+import main from './components/main.vue';
 </script>
 
 <style></style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,4 +1,19 @@
-<template></template>
+<template>
+    <header class="header">
+        <div class="container">
+            <div class="nav_content">
+                <div class="logo">
+                    <img src="./assets/imgs/logo (1).png" alt="">
+                    <ul class="nav_list">
+                        <li class="nav_list"><a href="#">a</a></li>
+                        <li class="nav_list"><a href="#"></a></li>
+                        <li class="nav_list"><a href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
 
 <script setup>
 
